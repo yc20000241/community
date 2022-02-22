c090c6e5663006634314a9ca9e29274cd3245b8c
俞超 <510218929@qq.com>
2022-02-22 15:56:03 +0800

    add comment list

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,9 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.QuestionDTO;
+import com.example.demo.mapper.QuestionMapper;
 import com.example.demo.mapper.UserMapper;
+import com.example.demo.model.Question;
 import com.example.demo.model.User;
+import com.example.demo.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private QuestionService questionService;
+
     @GetMapping("/")
-    public String index(HttpServletRequest request){
+    public String index(HttpServletRequest request,
+                        Model model){
         Cookie[] cookies = request.getCookies();
         if(cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
@@ -32,6 +42,9 @@ public class IndexController {
                 }
             }
         }
+
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions",questionList);
         return "index";
     }
 }
--- src/main/java/com/example/demo/mapper/QuestionMapper.java
+++ src/main/java/com/example/demo/mapper/QuestionMapper.java
@@ -3,11 +3,17 @@ package com.example.demo.mapper;
 import com.example.demo.model.Question;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Mapper
 @Component
 public interface QuestionMapper {
+
+    @Select("select * from question")
+    List<Question> list();
 
     @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag) values(#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
     public void create(Question question);
